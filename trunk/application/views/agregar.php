061b60141b51a6933fce3b7a9b6a9b52ef338280
nahuel9000 <nahuel9000@db972326-0cc5-c585-5786-88119e52df55>
2015-02-10 16:40:05 +0000

    selectpicker en agregar

--- trunk/application/views/agregar.php
+++ trunk/application/views/agregar.php
@@ -8,10 +8,11 @@
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
   <title>RedInmo</title>
 
-  <link href="<?php echo base_url();?>theme/dist/css/bootstrap.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>theme/dist/css/propio.css" rel="stylesheet">  
-  <link href="<?php echo base_url();?>theme/css/navbar-fixed-top.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>theme/css/sticky-footer.css" rel="stylesheet">  
+    <link href="<?php echo base_url();?>theme/dist/css/bootstrap-select.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>theme/dist/css/bootstrap.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>theme/dist/css/propio.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>theme/css/navbar-fixed-top.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>theme/css/sticky-footer.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="<?php echo base_url(); ?>theme/assets/js/html5shiv.js"></script>
@@ -33,19 +34,30 @@
 <form name="agregar_aviso" method="post" action="<?php echo base_url();?>avisos/agregar">
 <div class="form-group">
 <div class="input-group">
-<span class="input-group-addon" style="min-width:180px">Tipo de operacion</span>
-<select name="tipoop" id="tipoop" class="form-control" data-toggle="popover" data-trigger="hover" data-content="Las ventas se hacen en dolares y los alquileres en pesos">
-  <option value="0">Venta</option>
-  <option value="1">Alquiler</option>
+<span class="input-group-addon" style="min-width:180px">Tipo de operaci&oacute;n:</span>
+<select name="tipoop" id="tipoop" class="selectpicker show-tick form-control" data-toggle="popover" data-trigger="hover" data-content="Las ventas se hacen en dolares y los alquileres en pesos">
+  <option value="1">Venta</option>
+  <option value="2">Alquiler</option>
+  <option value="3">Alquiler temporario</option>
 </select>
 </div>
 <br>
 <div class="input-group">
 <span class="input-group-addon" style="min-width:180px">Tipo de inmueble:</span>
-<select name="tipoinm" id="tipoinm" class="form-control">
-  <option value="0">Casa</option>
-  <option value="1">Departamento</option>
-  <option value="2">Cochera</option>
+<select name="tipoinm" id="tipoinm" class="selectpicker show-tick form-control">
+    <option value="1">Casa</option>
+    <option value="3">Departamento</option>
+    <option value="2">Garage</option>
+    <option value="4">PH</option>
+    <option value="5">Countrie</option>
+    <option value="6">Quinta</option>
+    <option value="7">Terreno y/o lote</option>
+    <option value="8">Campo y/o chacra</option>
+    <option value="9">Local comercial</option>
+    <option value="10">Negocio y/o fondo de comercio</option>
+    <option value="11">Oficina</option>
+    <option value="12">Consultorio</option>
+    <option value="13">Boveda, nichos y/o parcela</option>
 </select>
 </div>
 </div>
@@ -53,7 +65,7 @@
 <div class="form-group">
 <div class="input-group">
 <span class="input-group-addon" style="min-width:180px">Provincia:</span>
-<select name="provincia" id="provincia" class="form-control">
+<select name="provincia" id="provincia" class="selectpicker show-tick form-control">
   <option>Santa Fe</option>
   <option>Buenos Aires</option>
 </select>
@@ -70,7 +82,7 @@
 </div>
 <br>
 <div class="input-group">
-<span class="input-group-addon" style="min-width:180px">Dirección:</span>
+<span class="input-group-addon" style="min-width:180px">Direcci&oacute;n:</span>
 <input name="direccion" type="text" id="direccion" value="" class="form-control" data-toggle="popover" data-trigger="hover" data-content="Direccion exacta para ubicar en google maps.">
 </div>
 <br>
@@ -92,7 +104,7 @@
 		</div>
         <div class="col-md-4">
 			<div class="input-group">
-			<span class="input-group-addon" style="min-width:100px">Baños:</span>
+			<span class="input-group-addon" style="min-width:100px">Ba&ntilde;os:</span>
 			<input name="banios" type="text" id="banios" value="" class="form-control">
 			</div>	
 		</div>		
@@ -101,7 +113,7 @@
       <div class="row">
         <div class="col-md-4">
 			<div class="input-group">
-			<span class="input-group-addon" style="min-width:100px">Metros2: </span>
+			<span class="input-group-addon" style="min-width:100px">Metros cuadrados: </span>
 			<input name="metros2" type="text" id="metros2" value="" class="form-control">
 			</div>	
 		</div>
@@ -113,7 +125,7 @@
 		</div>
         <div class="col-md-4">
 			<div class="input-group">
-			<span class="input-group-addon" style="min-width:100px">Año:</span>
+			<span class="input-group-addon" style="min-width:100px">A&ntilde;o:</span>
 			<input name="anio" type="text" id="anio" value="" class="form-control">
 			</div>
 		</div>		
@@ -145,6 +157,7 @@ fotos<br><br>
 
     <script src="<?php echo base_url();?>theme/assets/js/jquery.js"></script>
     <script src="<?php echo base_url();?>theme/dist/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>theme/dist/js/bootstrap-select.js"></script>
 	<script type="text/javascript">
 	$('#direccion').popover();
 	$('#tipoop').popover();
